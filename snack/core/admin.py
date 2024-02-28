45984f35e17b38bd1171486663a7172eaae29e81
rzs <site.administrator@rzsgymn.ukr.education>
2024-02-28 12:03:47 +0200

    add json

--- snack/core/admin.py
+++ snack/core/admin.py
@@ -5,9 +5,7 @@ from django.contrib import admin
 from .models import Profile
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.models import User
-from .models import ToDo
 
-admin.site.register(ToDo)
 
 class ProfileInline(admin.StackedInline):
     model = Profile
